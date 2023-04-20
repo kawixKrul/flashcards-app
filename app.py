5ccdae7e92680ce453a1db4d4b359b52de5b5f6e
kawixKrul <macioukos@gmail.com>
2023-04-20 14:29:53 +0200

    initialize

---
+++ app.py
@@ -0,0 +1,97 @@
+from flask import Flask, render_template, redirect, url_for, flash, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user, UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user is None:
+            user = User(username=username, password=password)
+            db.session.add(user)
+            db.session.commit()
+            flash('You have successfully signed up!', 'success')
+            login_user(user)
+            return redirect(url_for('my_profile'))
+        else:
+            flash('That username is already taken.', 'error')
+
+    return render_template('signup.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user is not None and check_password_hash(user.password_hash, password):
+            login_user(user)
+            print('login succesful')
+            return redirect(url_for('my_profile'))
+        else:
+            flash('Invalid username or password.', 'error')
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/my-profile', methods=['GET', 'POST'])
+@login_required
+def my_profile():
+    return render_template(my_profile, user=current_user)
+
+
+@app.route('/')
+def home():  # put application's code here
+    return render_template('base.html')
+
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
